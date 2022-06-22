7c620a683f385250b82a3baf4128d403c557ea2f
Tom Gaunt <t0mrg@users.noreply.github.com>
2022-06-22 23:07:18 +0100

    Add pair comparison

--- scripts/manmap.py
+++ scripts/manmap.py
@@ -4,47 +4,122 @@ Functions for review of manual mapping
 
 import requests as req
 import pandas as pd
+import numpy as np
 
-def getspreadsheet(sheetid,sheetname):
+
+def getspreadsheet(sheetid, sheetname):
     """
     Receives a Google Sheets URL, downloads to a dataframe and returns this
     """
     url = f"https://docs.google.com/spreadsheets/d/{sheetid}/gviz/tq?tqx=out:csv&sheet={sheetname}"
-    gs_df = pd.read_csv(url, keep_default_na=False)
+    gs_df = pd.read_csv(url, keep_default_na=False)  # Read Google sheet as csv
     result = parse_df(gs_df)
-    return(result)
+    return result
+
 
 def parse_df(df):
     """
     Parse the dataframe to remove issues and harmonise terms
     """
     if "Exact non-EFO" not in list(df.columns):
-        df["Exact non-EFO"] = df['Exact'].astype(str) +"-"+ df["non-EFO"].astype(str)
+        df["Exact non-EFO"] = df["Exact"].astype(str) + "-" + df["non-EFO"].astype(str)
     if "inadequate" not in list(df.columns):
         df["inadequate"] = ""
-    for column in ["exact mapping","broad mapping","narrow mapping","inadequate","incorrect","Exact non-EFO"]:
-        df[column] = df[column].apply(lambda x: "" if str(x) == "-" or len(str(x)) == 0 else x)
-        df[column] = df[column].apply(lambda x: str(x).replace(","," ").split())
-        df[column] = df[column].apply(remove_url_prefix)
-    for column in ["Identifier1","Identifier2","Identifier3"]:
-        df[column] = df[column].apply(remove_url_prefix)
-    return(df)
+    for column in [
+        "exact mapping",
+        "broad mapping",
+        "narrow mapping",
+        "inadequate",
+        "incorrect",
+        "Exact non-EFO",
+    ]:
+        df[column] = df[column].apply(
+            lambda x: "" if str(x) == "-" or len(str(x)) == 0 else x
+        )  # set null values to empty
+        df[column] = df[column].apply(
+            lambda x: str(x).replace(",", " ").split()
+        )  # convert comma- or space-delimited strings to lists
+        df[column] = df[column].apply(
+            remove_url_prefix
+        )  # remove url prefix from each item in list
+    for column in ["Identifier1", "Identifier2", "Identifier3"]:
+        df[column] = df[column].apply(
+            remove_url_prefix
+        )  # remove url prefix from auto-tool mappings
+    return df
+
 
 def remove_url_prefix(col):
     """
     Remove URLs, just keeping last item
     """
-    if not isinstance(col, list):
+    if not isinstance(col, list):  # Automatic tool mapping columns only have 1 ID
         col = [col]
     if len(col) > 0:
         res = []
         for item in col:
             try:
-                res.append(item.rsplit("/",1)[1])
+                res.append(
+                    item.rsplit("/", 1)[1]
+                )  # Retain only the right-hand part of URL after last "/"
             except IndexError:
                 res.append(item)
     else:
         res = col
-    return(res)
+    return res
 
 
+def pair_compare(mappingdf1, mappingdf2):
+    """
+    Compare two mapping efforts
+    """
+    targetcolumns = {
+        "exact mapping": 0,
+        "broad mapping": 1,
+        "narrow mapping": 2,
+        "inadequate": 3,
+        "incorrect": 4,
+        "Exact non-EFO": 5,
+    }
+    sourcecolumns = ["Identifier1", "Identifier2", "Identifier3"]
+    pairdf = pd.merge(
+        mappingdf1,
+        mappingdf2,
+        how="inner",
+        left_on="OpenGWAS.Trait",
+        right_on="OpenGWAS.Trait",
+        suffixes=("_x", "_y"),
+    )
+    tool1, tool2, tool3 = np.zeros((6, 6)), np.zeros((6, 6)), np.zeros((6, 6))
+    for index, row in pairdf.iterrows():
+        for sourcecolumn in sourcecolumns:
+            for targetcolumnx in targetcolumns.keys():
+                a = row[targetcolumnx + "_x"]
+                for targetcolumny in targetcolumns.keys():
+                    b = row[targetcolumny + "_y"]
+                    if (
+                        row[sourcecolumn + "_y"][0] in a
+                        and row[sourcecolumn + "_y"][0] in b
+                    ):
+                        if sourcecolumn == "Identifier1":
+                            tool1[
+                                targetcolumns[targetcolumnx],
+                                targetcolumns[targetcolumny],
+                            ] += 1
+                        elif sourcecolumn == "Identifier2":
+                            tool3[
+                                targetcolumns[targetcolumnx],
+                                targetcolumns[targetcolumny],
+                            ] += 1
+                        elif sourcecolumn == "Identifier3":
+                            tool3[
+                                targetcolumns[targetcolumnx],
+                                targetcolumns[targetcolumny],
+                            ] += 1
+    return (tool1, tool2, tool3)
+
+
+def person_summary(mappingdf):
+    """
+    Summarise the results from one person
+    """
--- scripts/manualmapping_review.py
+++ scripts/manualmapping_review.py
@@ -3,22 +3,36 @@ Python script to review and summarise the initial manual mapping exercise
 for OpenGWAS traits to EFO
 """
 
-# Libraries
+# Libraries
 
 from manmap import *
 
-sheet_ids = {"Benjamin":"1d32FA5-Yw-1MQFMX5ONBPCMWryt49THnROL2AS8u5hw",
-        "Rebecca":"1JDv-ycajsd-32W2tigyd5hdNd9PXuasBbOHJdx0G7fs",
-        "Samuel":"1i0LevfdgQaRkmPnCLA8k1pzmyvI1Y4jd1bBFPh3FH0Y",
-        "Ryan":"1BH0xST8Iqe4hazhadLVmxduzGTxvKnSwXJcoaiRdeH4",
-        "Maria":"1zAldGEGRb3H0apLO529i7JqM7QnsZ_z4",
-        "Zak":"1fxgEGT34gHdRuUhazWNddUETMd5GLzgoJUbQ1M8C__o",
-        "Marina":"1VVWSo_4gvp3JmIy5ycC9RfXFc1zUPf-0Uk-isoyAi-k",
-        "Giulio":"1aYOWaNoO37eOzI3ZCXrEF1EmcgujW-Z9Jn7KnlmY_8A"}
+sheet_ids = {
+    "Benjamin": "1d32FA5-Yw-1MQFMX5ONBPCMWryt49THnROL2AS8u5hw",
+    "Rebecca": "1JDv-ycajsd-32W2tigyd5hdNd9PXuasBbOHJdx0G7fs",
+    "Samuel": "1i0LevfdgQaRkmPnCLA8k1pzmyvI1Y4jd1bBFPh3FH0Y",
+    "Ryan": "1BH0xST8Iqe4hazhadLVmxduzGTxvKnSwXJcoaiRdeH4",
+    "Maria": "1zAldGEGRb3H0apLO529i7JqM7QnsZ_z4",
+    "Zak": "1fxgEGT34gHdRuUhazWNddUETMd5GLzgoJUbQ1M8C__o",
+    "Marina": "1VVWSo_4gvp3JmIy5ycC9RfXFc1zUPf-0Uk-isoyAi-k",
+    "Giulio": "1aYOWaNoO37eOzI3ZCXrEF1EmcgujW-Z9Jn7KnlmY_8A",
+}
+pairs = [("Rebecca", "Samuel"), ("Zak", "Marina")]
+sheets = {}
 
 for id in sheet_ids.keys():
-    print(id)
-    res = getspreadsheet(sheet_ids[id],"assigned_traits")
-    res.to_csv(id+".csv")
+    res = getspreadsheet(sheet_ids[id], "assigned_traits")
+    sheets[id] = res
 
+column_labels = ["Exact", "Broad", "Narrow", "Inad", "Inex", "non-EFO"]
 
+for pair in pairs:
+    print(f"Pair: {pair[0]}, {pair[1]}\n=========================")
+    tool1, tool2, tool3 = pair_compare(sheets[pair[0]], sheets[pair[1]])
+    print("\nAutomated tool 1")
+    print(pd.DataFrame(tool1, columns=column_labels, index=column_labels))
+    print("\nAutomated tool 2")
+    print(pd.DataFrame(tool2, columns=column_labels, index=column_labels))
+    print("\nAutomated tool 3")
+    print(pd.DataFrame(tool3, columns=column_labels, index=column_labels))
+    print("=============================================\n")
